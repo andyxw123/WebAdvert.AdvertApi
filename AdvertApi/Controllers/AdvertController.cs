a02d5e5b7654a53555197eff9801e7f4aed54f76
Andy W <andyxwright@yahoo.co.uk>
2020-07-27 16:08:45 +0100

    End of section 18

---
+++ AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("adverts/v1")]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            this._advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(typeof(CreateAdvertResponse), 200)]
+        public async IActionResult Create(AdvertModel model)
+        {
+            
+        }
+    }
+}
